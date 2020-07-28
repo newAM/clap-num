89eed056e648d75c97de690745d22eab227c745b
Alex M <alexmgit@protonmail.com>
2020-07-27 17:53:48 -0700

    Added validate_integer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,78 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub use clap;
+use core::str::FromStr;
+
+/// Validate a signed or unsigned integer value.
+///
+/// # Arguments
+///
+/// * `s` - String to parse.
+/// * `min` - Minimum value, inclusive.
+/// * `max` - Maximum value, inclusive.
+///
+/// # Example
+///
+/// This allows for a number of cents to be passed in the range of 0-99
+/// (inclusive).
+///
+/// ```
+/// use clap::Clap;
+/// use clap_validators::validate_integer;
+///
+/// fn less_than_100(s: &str) -> Result<u8, String> {
+///     validate_integer(s, 0, 99)
+/// }
+///
+/// #[derive(Clap, Debug)]
+/// struct Change {
+///     #[clap(long, parse(try_from_str=less_than_100))]
+///     cents: u8,
+/// }
+/// #
+/// # fn main() {
+/// #   let args = Change::parse_from(&["", "--cents", "99"]);
+/// #   assert_eq!(args.cents, 99);
+/// # }
+/// ```
+///
+/// ## Error Messages
+///
+/// Values exceeding this range will show an error message similar to this:
+///
+/// ```bash
+/// error: Invalid value for '--cents <cents>': exceeds maximum of 99
+/// ```
+///
+/// Values that are not numbers will show an error message similar to this:
+///
+/// ```bash
+/// error: Invalid value for '--cents <cents>': invalid digit found in string
+/// ```
+///
+/// Values result in integer overflow will show an error message like this:
+///
+/// ```bash
+/// error: Invalid value for '--cents <cents>': number too large to fit in target type
+/// ```
+pub fn validate_integer<T: Ord + PartialOrd + std::fmt::Display>(
+    s: &str,
+    min: T,
+    max: T,
+) -> Result<T, String>
+where
+    T: FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Display,
+{
+    debug_assert!(min <= max, "minimum of {} exceeds maximum of {}", min, max);
+    match s.parse::<T>() {
+        Ok(v) => {
+            if v > max {
+                Err(format!("exceeds maximum of {}", max))
+            } else if v < min {
+                Err(format!("exceeds minimum of {}", min))
+            } else {
+                Ok(v)
+            }
+        }
+        Err(e) => Err(format!("{}", e)),
     }
 }
---
+++ tests/validate_integer.rs
@@ -0,0 +1,108 @@
+use clap::Clap;
+use clap_validators::validate_integer;
+
+// standalone basic tests
+#[cfg(test)]
+mod basic {
+    use super::*;
+
+    macro_rules! pos {
+        ($NAME:ident, $VAL:expr, $MIN:expr, $MAX:expr, $RESULT:expr) => {
+            #[test]
+            fn $NAME() {
+                assert_eq!(validate_integer($VAL, $MIN, $MAX), Ok($RESULT));
+            }
+        };
+    }
+
+    macro_rules! neg {
+        ($NAME:ident, $VAL:expr, $MIN:expr, $MAX:expr, $RESULT:expr) => {
+            #[test]
+            fn $NAME() {
+                assert_eq!(
+                    validate_integer($VAL, $MIN, $MAX),
+                    Err(String::from($RESULT))
+                );
+            }
+        };
+    }
+
+    pos!(simple, "123", 12u8, 200u8, 123u8);
+    pos!(zero, "0", 0u8, 0u8, 0u8);
+    pos!(neg, "-1", -10i8, 10i8, -1);
+    pos!(min_limit, "-5", -5i8, -5i8, -5i8);
+    pos!(max_limit, "65535", 0, std::u16::MAX, std::u16::MAX);
+
+    neg!(min, "-1", 0i8, 0i8, "exceeds minimum of 0");
+    neg!(max, "1", 0i8, 0i8, "exceeds maximum of 0");
+    neg!(
+        overflow,
+        "256",
+        0,
+        std::u8::MAX,
+        "number too large to fit in target type"
+    );
+    neg!(nan, "nan", 0, 0, "invalid digit found in string");
+
+    #[test]
+    #[should_panic]
+    fn min_max_debug_assert() {
+        let _ = validate_integer("", 2, 1);
+    }
+}
+
+// integration tests with clap
+#[cfg(test)]
+mod integration {
+    use super::*;
+
+    fn human_livable_temperature(s: &str) -> Result<i8, String> {
+        validate_integer(s, -40, 60)
+    }
+
+    #[derive(Clap, Debug)]
+    struct Thermostat {
+        #[clap(
+            long,
+            parse(try_from_str=human_livable_temperature),
+            allow_hyphen_values=true
+        )]
+        temperature: i8,
+    }
+
+    // positive path
+    macro_rules! pos {
+        ($NAME:ident, $VAL:expr, $RESULT:expr) => {
+            #[test]
+            fn $NAME() {
+                let opt = Thermostat::parse_from(&["", "--temperature", $VAL]);
+                assert_eq!(opt.temperature, $RESULT);
+            }
+        };
+    }
+
+    // negative path
+    macro_rules! neg {
+        ($NAME:ident, $VAL:expr, $RESULT:expr) => {
+            #[test]
+            fn $NAME() {
+                let opt = Thermostat::try_parse_from(&["", "--temperature", $VAL]);
+                match opt {
+                    Err(e) => {
+                        assert!(format!("{:?}", e).contains($RESULT));
+                    }
+                    _ => unreachable!(),
+                };
+            }
+        };
+    }
+
+    pos!(simple, "50", 50);
+    pos!(zero, "0", 0);
+    pos!(negative, "-30", -30);
+    pos!(positive_limit, "60", 60);
+    pos!(negative_limit, "-40", -40);
+
+    neg!(too_small, "-41", "exceeds minimum of -40");
+    neg!(too_large, "61", "exceeds maximum of 60");
+}
