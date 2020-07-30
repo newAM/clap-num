7c32c1028a7276c0f94039426a967486afd2b887
Alex M <alexmgit@protonmail.com>
2020-07-29 20:55:12 -0700

    Add links to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@
 //! ```
 //!
 //! [clap v3]: https://github.com/clap-rs/clap
-#![doc(html_root_url = "https://docs.rs/clap-num/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/clap-num/0.1.2")]
 #![deny(warnings, missing_docs)]
 
 use core::convert::TryFrom;
