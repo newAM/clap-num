6ade163df6ff07c013cb40752b11b5494d7cfe40
Alex Martens <alex@thinglab.org>
2025-01-24 07:45:24 -0800

    lib.rs: backport changes from README

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,13 @@
 //! strings provided by [clap].
 //!
 //! * `maybe_hex`
-//!   Validates an unsigned integer value that can be base-10 or base-16.
+//!   Validate an unsigned integer value that can be base-10 or base-16.
 //! * `maybe_hex_range`
-//!   Validates an unsigned integer value that can be base-10 or base-16 within a range.
+//!   Validate an unsigned integer value that can be base-10 or base-16 within a range.
+//! * `maybe_bin`
+//!   Validate an unsigned integer value that can be base-10 or base-2.
+//! * `maybe_bin_range`
+//!   Validate an unsigned integer value that can be base-10 or base-2 within a range.
 //! * `number_range`
 //!   Validate a signed or unsigned integer value.
 //! * `si_number`
